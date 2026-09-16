afb4a73f3c9cbb3d109194dee1f21b9b193efdcb
Eyanla Garcia <eygarcia@iu.edu>
2026-09-15 23:40:59 -0400

    hw2

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { PrismaClient } from "../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
-import task from "./task.json" with { type: "json" };
+import task from "./tasks-seed.json" with { type: "json" };
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 const prisma = new PrismaClient({ adapter });
